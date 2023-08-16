41e585102d91cbf894327ace23a26745b8b3bb59
Ümit Şimşek <umitsimsek002@gmail.com>
2023-08-16 08:47:14 +0300

    counting-sort-2 solved

---
+++ Hackerrank.playground/Pages/counting-sort-2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+/*
+ * Complete the 'countingSort' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+func countingSort(arr: [Int]) -> [Int] {
+    // Write your code here
+    return arr.sorted(by: <)
+}
